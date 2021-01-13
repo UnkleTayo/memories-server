5a271bee4eb1938b6eeca84cfc50398485ebe471
Adetayo <akinsanyaadetayo7@gmail.com>
2021-01-13 11:46:50 +0100

    push to heroku

---
+++ controllers/posts.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+  try {
+    const postsMesssage = await PostMessage.find();
+    res.status(200).json(postsMesssage);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(490).json({ message: err.message });
+  }
+};
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  // check if id is the object setting
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send('No post with that Id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('No post found with that id');
+
+  await PostMessage.findByIdAndRemove(id);
+  res.json({ message: 'Post sucessfully deleted' });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('No post with the given Id');
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    {
+      likeCount: post.likeCount + 1,
+    },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};
